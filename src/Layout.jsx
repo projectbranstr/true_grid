413a0275618c5fc57304034e28a80fd0070fa42b
Amanx08 <amanx03590@gmail.com>
2025-02-08 05:24:56 +0530

    create article page

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Outlet } from "react-router-dom";
-import Header from './component/molecule/Header/Header';
+import Header from './component/molecule/Header/Header.jsx';
 
 function Layout() {
     return (
--- src/pages/Article.jsx
+++ src/pages/Article.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Articlebanner from '../component/sectiontemplate/Articlebanner/Articlebanner'
+import ArticleSection from '../component/sectiontemplate/Articlesection/ArticleSection'
 
 function Article() {
     return (
         <div>
-            About
+            <Articlebanner />
+            <ArticleSection />
         </div>
     )
 }
