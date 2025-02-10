85967e3e59a7b67235e3df6a31d6c71c014aba17
Amanx08 <amanx03590@gmail.com>
2025-02-10 05:38:22 +0530

    add home pages section

--- src/component/sectiontemplate/Mainbanner/Mainbanner.jsx
+++ src/component/sectiontemplate/Mainbanner/Mainbanner.jsx
@@ -3,20 +3,42 @@ import './mainbanner.scss'
 function Mainbanner() {
     return (
         <>
-            <div className="jsx_main_container">
-                <div className="jsx_banner_heading">
-                    Authenticity in every Chart
-                    Integrity in every Number
-                </div>
-                <div className="jsx_banner_searchbar_container">
-                    <input type="text" placeholder='Search...' />
-                    <img src='/img/search_icon.png' alt='search_icon' />
-                </div>
-                <div className="jsx_companyname_maincontainer">
-                    <button className="jsx_companyname_container">
-                        <img src="/img/teslalogo.png" alt="" className="" />
-                        TSLA
-                    </button>
+            <div className="jsx_banner_primary_container">
+                <div className="jsx_bannermain_container">
+                    <div className="jsx_banner_heading">
+                        Authenticity in every Chart
+                        Integrity in every Number
+                    </div>
+                    <div className="jsx_banner_searchbar_container">
+                        <input type="text" placeholder='Search...' />
+                        <img src='/img/search_icon.png' alt='search_icon' />
+                    </div>
+                    <div className="jsx_companyname_maincontainer">
+
+
+                        <button className="jsx_companyname_container">
+                            <img src="/img/teslalogo.png" alt="" className="" />
+                            TSLA
+                        </button>
+                        <button className="jsx_companyname_container">
+                            <img src="/img/teslalogo.png" alt="" className="" />
+                            TSLA
+                        </button><button className="jsx_companyname_container">
+                            <img src="/img/teslalogo.png" alt="" className="" />
+                            TSLA
+                        </button><button className="jsx_companyname_container">
+                            <img src="/img/teslalogo.png" alt="" className="" />
+                            TSLA
+                        </button><button className="jsx_companyname_container">
+                            <img src="/img/teslalogo.png" alt="" className="" />
+                            TSLA
+                        </button><button className="jsx_companyname_container">
+                            <img src="/img/teslalogo.png" alt="" className="" />
+                            TSLA
+                        </button>
+
+
+                    </div>
                 </div>
             </div>
         </>
---
+++ src/component/sectiontemplate/Testimonial/Testimonial.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react'
+import './Testimonial.scss'
+
+const testimonials = [
+    {
+        quote: "This is the best service I have ever used!",
+        author: "John Doe",
+        position: "CEO, Company X",
+    },
+    {
+        quote: "Absolutely wonderful experience!",
+        author: "Jane Smith",
+        position: "Marketing Head, Company Y",
+    },
+    {
+        quote: "I highly recommend this to everyone.",
+        author: "Alice Johnson",
+        position: "Product Manager, Company Z",
+    },
+];
+
+function Testimonial() {
+
+
+    return (
+        <>
+
+            <div className="jsx_primary_container">
+                <div class="jsx_testimonial-container">
+                    <div className="jsx_test_img_container">
+                        <img src="/img/avatar.png" alt="Tetimonial_img" className="" />
+                    </div>
+                    <div className="jsx_author_container">
+                        Howard Marks
+                    </div>
+                    <div className="jsx_quote_container">
+                        <sup> <img src="/img/quotestart.png" alt="quote_start" className="" /></sup>
+                        &nbsp;There's only one intelligent form of investing: figuring out what something's worth and see if you can buy it at or below that price. It's all about value
+                        &nbsp;<sup><img src="/img/quoteend.png" alt="quote_start" className="" /></sup>
+
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default Testimonial
